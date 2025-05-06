808c90171d4c2ed74081bee1e9a8216590e355e4
amin dehnavi <amin1384dehnavi@gmail.com>
2025-05-06 12:52:24 +0330

    ➕feat(mongoDB): create mongoDB

---
+++ src/lib/mongodb.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const connectMongoDB = async () => {
+    if (mongoose.connection.readyState >= 1) {
+        return;
+    }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        throw new Error('Failed to connect to MongoDB');
+    }
+};
+
+export default connectMongoDB;
