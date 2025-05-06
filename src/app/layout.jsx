f57cc7bfa44ed834c4f5fdea9f1fb03fcbe446c5
amin dehnavi <amin1384dehnavi@gmail.com>
2025-05-06 15:36:18 +0330

    ➕feat(themeProvider): create themeProvider

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,8 @@ import Footer from '../components/Footer';
 import '@/app/globals.css';
 import { AuthProvider } from '@/context/AuthContext';
 import { ToastContainer } from 'react-toastify';
+import { ThemeProvider } from '@mui/material';
+import { themeProvider } from '@/components/ThemeProvider';
 
 export const metadata = {
   title: 'سامانه نوبت‌دهی آرایشگاه',
@@ -13,12 +15,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className="min-h-screen flex flex-col">
-        <AuthProvider>
-          <Header />
-          <main className="flex-grow">{children}</main>
-          <Footer />
-        </AuthProvider>
-        <ToastContainer position="bottom-right" autoClose={3000} />
+        <ThemeProvider theme={themeProvider}>
+          <AuthProvider>
+            <Header />
+            <main className="flex-grow">{children}</main>
+            <Footer />
+          </AuthProvider>
+          <ToastContainer position="bottom-right" autoClose={3000} />
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ src/components/ThemeProvider.jsx
@@ -0,0 +1,8 @@
+"use client";
+import { createTheme } from '@mui/material/styles';
+
+export const themeProvider = createTheme({
+    typography: {
+        fontFamily: 'IRANSans, sans-serif',
+    },
+});
