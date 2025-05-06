9eabb196348e348b27cfb7f33f2164bd49ea27e0
amin dehnavi <amin1384dehnavi@gmail.com>
2025-05-06 18:13:28 +0330

    🔧fix(cookies): add cookies to auth.js

--- src/lib/routes/auth.js
+++ src/lib/routes/auth.js
@@ -1,3 +1,4 @@
+import Cookies from "js-cookie";
 import api from "./axiosInterceptor";
 
 export const register = async (data) => {
@@ -8,16 +9,16 @@ export const register = async (data) => {
 export const login = async (data) => {
     const response = await api.post('/login', data);
     if (typeof window !== 'undefined' && response.data.token) {
-        window.localStorage.setItem('token', response.data.token);
-        window.localStorage.setItem('expires_in', response.data.expires_in || Date.now() + 86400000);
+        Cookies.set('token', response.data.token);
+        Cookies.set('expires_in', response.data.expires_in || Date.now() + 86400000);
     }
     return response.data;
 };
 
 export const logout = async () => {
     if (typeof window !== 'undefined') {
-        window.localStorage.removeItem('token');
-        window.localStorage.removeItem('expires_in');
+        Cookies.remove('token');
+        Cookies.remove('expires_in');
     }
     return { message: 'خروج با موفقیت انجام شد' };
 };
