7207c3cb3374f959b47cc859a5d7308fb397a968
amin dehnavi <amin1384dehnavi@gmail.com>
2025-05-06 09:59:04 +0330

    ➕feat(authContext): create AuthCountetx

---
+++ src/context/AuthContext.js
@@ -0,0 +1,24 @@
+import { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [role, setRole] = useState(null);
+
+    const login = (userData, userRole) => {
+        setUser(userData);
+        setRole(userRole);
+    };
+
+    const logout = () => {
+        setUser(null);
+        setRole(null);
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, role, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
