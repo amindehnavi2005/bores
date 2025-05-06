d82873a9570fc70145b721ae0ad03e90a2fd5da8
amin dehnavi <amin1384dehnavi@gmail.com>
2025-05-06 12:52:10 +0330

    ➕feat(User): create user route

---
+++ src/api/(common)/register/route.js
@@ -0,0 +1,35 @@
+import bcrypt from 'bcryptjs';
+import User from '@/models/User';
+import { NextResponse } from 'next/server';
+import connectMongoDB from '@/lib/mongodb';
+
+export async function POST(request) {
+    try {
+        const { username, phone, password } = await request.json();
+
+        // اعتبارسنجی ورودی‌ها
+        if (!username || !phone || !password) {
+            return NextResponse.json({ message: 'تمامی فیلدها الزامی هستند' }, { status: 400 });
+        }
+
+        // اتصال به MongoDB
+        await connectMongoDB();
+
+        // بررسی وجود کاربر
+        const userExists = await User.findOne({ $or: [{ username }, { phone }] });
+        if (userExists) {
+            return NextResponse.json({ message: 'کاربر یا شماره تلفن قبلاً ثبت شده است' }, { status: 400 });
+        }
+
+        // هش کردن رمز عبور
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // ایجاد کاربر جدید
+        const newUser = new User({ username, phone, password: hashedPassword });
+        await newUser.save();
+
+        return NextResponse.json({ message: 'ثبت‌نام با موفقیت انجام شد' }, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: 'خطا در ثبت‌نام' }, { status: 500 });
+    }
+}
