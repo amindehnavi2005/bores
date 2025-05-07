0879d68579c20bb46feb901951c07381a9fcb647
amin dehnavi <amin1384dehnavi@gmail.com>
2025-05-07 10:12:39 +0330

    ➕feat(Header): add usePath to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 "use client";
-import { useContext, useState } from 'react';
-import { AuthContext } from '../context/AuthContext';
 import Link from 'next/link';
 import { Button } from '@mui/material';
+import { useContext, useState } from 'react';
 import { UserDropdown } from './UserDropdown';
+import { usePathname } from 'next/navigation';
+import { AuthContext } from '../context/AuthContext';
 
 const Header = () => {
-    const { user, logout } = useContext(AuthContext);
+    const pathname = usePathname();
+    console.log("PathName => ", pathname)
+    const { user } = useContext(AuthContext);
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     const toggleMobileMenu = () => {
@@ -24,10 +27,10 @@ const Header = () => {
                 </div>
                 {/* Navigation links */}
                 <div className={`flex-col md:flex md:flex-row md:space-x-4 ${mobileMenuOpen ? 'flex' : 'hidden'} md:flex absolute md:static top-16 left-0 w-full md:w-auto bg-primary md:bg-transparent p-4 md:p-0`}>
-                    <Link href="/" className="hover:text-accent py-2 md:py-0">صفحه اصلی</Link>
-                    <Link href="/appointments" className="hover:text-accent py-2 md:py-0">لیست نوبت‌ها</Link>
-                    <Link href="/about" className="hover:text-accent py-2 md:py-0">درباره ما</Link>
-                    <Link href="/contact" className="hover:text-accent py-2 md:py-0">تماس با ما</Link>
+                    <Link href="/" className={`hover:text-secondary ${pathname == "/" && "text-accent"} py-2 md:py-0`}>صفحه اصلی</Link>
+                    <Link href="/appointments" className={`hover:text-secondary ${pathname == "/appointments" && "text-accent"} py-2 md:py-0`}>لیست نوبت‌ها</Link>
+                    <Link href="/about" className={`hover:text-secondary ${pathname == "/about" && "text-accent"} py-2 md:py-0`}>درباره ما</Link>
+                    <Link href="/contact" className={`hover:text-secondary ${pathname == "/contact" && "text-accent"} py-2 md:py-0`}>تماس با ما</Link>
                 </div>
                 {/* User menu */}
                 <div>
